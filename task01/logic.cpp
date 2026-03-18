47ba7410acf003a71f8d97d8905e9f572b0ef0e0
polin-sv <polinasvetlay@icloud.com>
2026-03-18 17:49:18 +0300

    update

--- task01/logic.cpp
+++ task01/logic.cpp
@@ -2,4 +2,17 @@
 
 void sort_bubble(int array[], int length) {
 	
+	for (int i = 0; i < length - 1; i++)
+	{
+		for (int j = 0; j < length - 1; j++)
+		{
+			if (array[j] > array[j + 1]) {
+				int t = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = t;
+			}
+		}
+	}
+	
+	
 }
--- task01/main.cpp
+++ task01/main.cpp
@@ -6,8 +6,18 @@ int main() {
 
 	cout << "input size of array: ";
 	cin >> size;
+	init_random(array, size, 0, 100);
 
+	print("befour sourting:\n");
 	print(convert(array, size));
+
+	sort_bubble(array, size);
+	print("\n\nafter:\n");
+	print(convert(array, size));
+
+
+	sort_bubble(array, size);
+
 
 	return 0;
 }
